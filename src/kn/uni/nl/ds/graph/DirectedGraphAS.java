0f06119d7d9feb2d493cd828ed0e17a4a3bc240f
Juergen Lerner <juergen.lerner@gmail.com>
2021-11-19 07:28:40 +0100

    Update DirectedGraphAS.java

--- src/kn/uni/nl/ds/graph/DirectedGraphAS.java
+++ src/kn/uni/nl/ds/graph/DirectedGraphAS.java
@@ -152,8 +152,8 @@ public class DirectedGraphAS<T> extends DirectedGraph<T> {
 	 * via an adjacency set representation and the code with the anonymous inner class 
 	 * would seem to be unduly complicated (compared with the code of all other methods).
 	 * 
-	 * Runtime of this method is in O(|E|+|V|). It would be possible to reduce the runtime
-	 * to O(|E|) by maintaining only non-empty sets of inNeighbors and outNeighbors. However,
+	 * Runtime of this method is in O(|E|+|V|+1). It would be possible to reduce the runtime
+	 * to O(|E|+1) by maintaining only non-empty sets of inNeighbors and outNeighbors. However,
 	 * this modification would be only relevant in very sparse graphs in which the number of
 	 * nodes grows asymptotically faster than the number of edges. The implementation of several
 	 * methods would have to be modified as well.
