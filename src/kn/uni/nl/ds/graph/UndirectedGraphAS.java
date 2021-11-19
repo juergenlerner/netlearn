ca83c65a3027f29ce4050fe9bbe5629831ed070f
Juergen Lerner <juergen.lerner@gmail.com>
2021-11-19 07:26:55 +0100

    Update UndirectedGraphAS.java

--- src/kn/uni/nl/ds/graph/UndirectedGraphAS.java
+++ src/kn/uni/nl/ds/graph/UndirectedGraphAS.java
@@ -141,7 +141,11 @@ public class UndirectedGraphAS<T> extends UndirectedGraph<T> {
 	 * via an adjacency set representation and the code with the anonymous inner class 
 	 * would seem to be unduly complicated (compared with the code of all other methods).
 	 * 
-	 * Runtime of this method is still in O(|E|) which is asymptotically optimal.
+	 * Runtime of this method is in O(|E|+|V|). It would be possible to reduce the runtime
+	 * to O(|E|) by maintaining only non-empty sets of neighbors. However,
+	 * this modification would be only relevant in very sparse graphs in which the number of
+	 * nodes grows asymptotically faster than the number of edges. The implementation of several
+	 * methods would have to be modified as well.
 	 * 
 	 * @return E
 	 */
